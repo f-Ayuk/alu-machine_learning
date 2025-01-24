871ad953b1c02fffb66e0e4fbc72f08fd2f5bde2
Favor Ayuk <127386070+f-Ayuk@users.noreply.github.com>
2025-01-24 15:03:00 +0200

    Update 2-size_me_please.py

--- math/linear_algebra/2-size_me_please.py
+++ math/linear_algebra/2-size_me_please.py
@@ -1,8 +1,14 @@
 #!/usr/bin/env python3
 
 def matrix_shape(matrix):
-    """
-    Calculates the shape of the matrix
+     """
+    Calculates the shape of a matrix.
+
+    Args:
+        matrix: The input matrix.
+
+    Returns:
+        A list of integers representing the shape of the matrix.
     """
     shape = []
     while type(matrix) is list:
