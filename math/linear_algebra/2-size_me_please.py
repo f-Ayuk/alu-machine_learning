84850d356f4643d5452973470435acd57414fde2
Favor Ayuk <127386070+f-Ayuk@users.noreply.github.com>
2025-01-24 22:25:12 +0200

    Update 2-size_me_please.py

--- math/linear_algebra/2-size_me_please.py
+++ math/linear_algebra/2-size_me_please.py
@@ -1,10 +1,8 @@
 #!/usr/bin/env python3
-
 def matrix_shape(matrix):
     """
     Calculates the shape of a matrix.
     """
-    
     shape = []
     while type(matrix) is list:
         shape.append(len(matrix))
