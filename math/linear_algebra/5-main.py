4bf658e9bb19c3ad054e6192e9b5e83e51c0a2da
Favor Ayuk <127386070+f-Ayuk@users.noreply.github.com>
2025-01-24 22:35:33 +0200

    Create 5-main.py

---
+++ math/linear_algebra/5-main.py
@@ -0,0 +1,5 @@
+#!/usr/bin/env python3
+mat1 = [[1, 2], [3, 4]]
+mat2 = [[5, 6], [7, 8]]
+print(add_matrices2D(mat1, mat2))
+print(add_matrices2D(mat1, [[1, 2, 3], [4, 5, 6]]))  # Output: None (different shapes)
