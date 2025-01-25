7cd5370d239d92a34aa2d194f969abd5cdb425e3
Favor Ayuk <127386070+f-Ayuk@users.noreply.github.com>
2025-01-25 14:36:30 +0200

    Update 7-gettin_cozy.py

--- math/linear_algebra/7-gettin_cozy.py
+++ math/linear_algebra/7-gettin_cozy.py
@@ -14,3 +14,17 @@ def cat_matrices2D(mat1, mat2, axis=0):
             return None
         return [matrix1[i] + matrix2[i] for i in range(len(matrix1))]
     return None
+
+
+# mat1 = [[1, 2], [3, 4]]
+# mat2 = [[5, 6],]
+# mat3 = [[7], [8]]
+# mat4 = cat_matrices2D(mat1, mat2)
+# mat5 = cat_matrices2D(mat1, mat3, axis=1)
+# print(mat4)
+# print(mat5)
+# mat1[0] = [9, 10]
+# mat1[1].append(5)
+# print(mat1)
+# print(mat4)
+# print(mat5)
