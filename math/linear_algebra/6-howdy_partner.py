1c63fa711b4eb458f6fa7a5056c37dfe23bd32c4
Favor Ayuk <127386070+f-Ayuk@users.noreply.github.com>
2025-01-25 13:11:58 +0200

    Update 6-howdy_partner.py

--- math/linear_algebra/6-howdy_partner.py
+++ math/linear_algebra/6-howdy_partner.py
@@ -1,13 +1,13 @@
 #!/usr/bin/env python3
 def cat_arrays(arr1, arr2):
-  """
-  Concatenates two arrays.
+    """
+    Concatenates two arrays.
 
-  Parameters:
-      arr1: The first input list.
-      arr2: The second input list.
+    Parameters:
+        arr1: The first input list.
+        arr2: The second input list.
 
-  Returns:
-      A new list containing the concatenated elements of arr1 and arr2.
-  """
-  return arr1 + arr2
+    Returns:
+        A new list containing the concatenated elements of arr1 and arr2.
+    """
+    return arr1 + arr2
