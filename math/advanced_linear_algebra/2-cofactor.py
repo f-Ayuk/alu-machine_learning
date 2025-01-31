d5d0877217be62d1e4141a31d03ac53fbe58c5a5
Favor Ayuk <127386070+f-Ayuk@users.noreply.github.com>
2025-01-31 22:15:42 +0200

    Update 2-cofactor.py

--- math/advanced_linear_algebra/2-cofactor.py
+++ math/advanced_linear_algebra/2-cofactor.py
@@ -13,7 +13,7 @@ def determinant(matrix):
         return matrix[0][0]
     if len(matrix) == 2:
         det = ((matrix[0][0] * matrix[1][1])
-                  - (matrix[0][1] * matrix[1][0]))
+        - (matrix[0][1] * matrix[1][0]))
         return det
     det = 0
     for i, j in enumerate(matrix[0]):
