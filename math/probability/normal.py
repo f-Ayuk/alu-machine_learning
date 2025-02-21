fc488b501c3619301538260960bffe86b5ecd37e
Favor Ayuk <127386070+f-Ayuk@users.noreply.github.com>
2025-02-21 21:45:13 +0200

    Update normal.py

--- math/probability/normal.py
+++ math/probability/normal.py
@@ -9,3 +9,18 @@ class Normal:
 
         self.E = 2.7182818285
         self.PI = 3.1415926536
+
+        if data is None:
+            if stddev <= 0:
+                raise ValueError('stddev must be a positive value')
+            else:
+                self.stddev = float(stddev)
+            self.mean = float(mean)
+        else:
+            if not isinstance(data, list):
+                raise TypeError('data must be a list')
+            if len(data) < 2:
+                raise ValueError('data must contain multiple values')
+            self.mean = (sum(data) / len(data))
+            self.stddev = (sum([(x - self.mean) ** 2 for x in data])
+                           / len(data)) ** 0.5
