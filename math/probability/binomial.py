d3d404153823cdeab337d7b7ee9f579f9aec1c95
Favor Ayuk <127386070+f-Ayuk@users.noreply.github.com>
2025-02-21 22:59:44 +0200

    Update binomial.py

--- math/probability/binomial.py
+++ math/probability/binomial.py
@@ -1,25 +1,30 @@
 #!/usr/bin/env python3
-"""creating a class that represents a poisson distribution."""
+"""create a class Binomial that represents a binomial distribution
+"""
 
 
-class Poisson:
-    """Creating the Poisson class
-    """
+class Binomial:
+    """Class Binomial"""
 
-    def __init__(self, data=None, lambtha=1.):
-        """ Constructor
-        """
-        self.E = 2.7182818285
-        self.PI = 3.1415926536
+    def __init__(self, data=None, n=1, p=0.5):
+        """Constructor"""
 
         if data is None:
-            if lambtha <= 0:
-                raise ValueError('lambtha must be a positive value')
-            else:
-                self.lambtha = float(lambtha)
+            if n <= 0:
+                raise ValueError('n must be a positive value')
+            if p <= 0 or p >= 1:
+                raise ValueError('p must be greater than 0 and less than 1')
+            self.n = int(n)
+            self.p = float(p)
+
         else:
             if not isinstance(data, list):
                 raise TypeError('data must be a list')
             if len(data) < 2:
                 raise ValueError('data must contain multiple values')
-            self.lambtha = float(sum(data) / len(data))
+            mean = float(sum(data) / len(data))
+            var = float((sum(map(lambda n: pow(n - mean,
+                        2), data)) / len(data)))
+            self.p = 1 - (var / mean)
+            self.n = round(mean / self.p)
+            self.p = mean / self.n
