bf607b57e65f8092f670962b11e5178d7d496799
Favor Ayuk <127386070+f-Ayuk@users.noreply.github.com>
2025-02-21 15:59:59 +0200

    Update normal.py

--- math/probability/normal.py
+++ math/probability/normal.py
@@ -1,1 +1,11 @@
+#!/usr/bin/env python3
+"""create a class Normal that represents a normal distribution"""
 
+
+class Normal:
+    """class Normal"""
+    def __init__(self, data=None, mean=0., stddev=1.):
+        """Constructor"""
+
+        self.E = 2.7182818285
+        self.PI = 3.1415926536
